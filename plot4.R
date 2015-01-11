8e28d5caefcdefced633c16222d3b135ac79b4bd
malbolg <malbolg@gmail.com>
2015-01-12 00:06:56 +0200

    test

--- plot4.R
+++ plot4.R
@@ -9,8 +9,7 @@ plot(D2$DateTime, D2$Sub_metering_1, type="l", ylab="Energy sub metering", xlab=
 lines(D2$DateTime, D2$Sub_metering_2, col="red")
 lines(D2$DateTime, D2$Sub_metering_3, col="blue")
 
-legend("topright", lty=1, legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black","red","blue"))
-
+legend("top", lty=1, bty="n", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black","red","blue"))
 
 
 plot(D2$DateTime, D2$Global_reactive_power, ylab="Global_reactive_power", xlab="datetime", type="l")
